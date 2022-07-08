ef056ef092cc2277872927f547561512f2a4afc0
NFT Yoginis <gabeyoga@gmail.com>
2022-07-08 07:13:54 +0700

    rinkeby test

--- backend/src/config.js
+++ backend/src/config.js
@@ -7,66 +7,393 @@ const { NETWORK } = require(`${basePath}/constants/network.js`);
 const network = NETWORK.eth;
 
 // General metadata for Ethereum
-const namePrefix = "YOUR COLLECTION NAME";
-const description = "Remember to replace this description";
-const baseUri = "ipfs://NewUriToReplace"; // This will be replaced automatically
+const namePrefix = "NFTYoginis Goddess Collection";
+const description = "Goddesses Collection to Inspire and Empower Holders in the Metaverse";
+const baseUri = "ipfs://QmNy6ZJcBVPTPHLLxcqLy125R6MNf5uGWbSKzwg1FU9VST";
 
+// If you have selected Solana then the collection starts from 0 automatically
 const layerConfigurations = [
   {
-    growEditionSizeTo: 5,
+    growEditionSizeTo: 150,
     layersOrder: [
-      { name: "Background" },
-      { name: "Eyeball" },
-      { name: "Eye color" },
-      { name: "Iris" },
-      { name: "Shine" },
-      { name: "Bottom lid" },
-      { name: "Top lid" },
+    { name: "Background" },
+    { name: "Halo" }, 
+    { name: "Base Skin Color" },
+    { name: "Mouth" },
+    { name: "Nose" },
+    { name: "Eyes" },
+    { name: "Hair Color" },
+    { name: "Extra Arm" },
+    { name: "Clothes" },
+    { name: "Hair Accessories" },
+    { name: "Accessories" },
     ],
-  },
+    },   
+    {
+      growEditionSizeTo: 280,
+      layersOrder: [
+      { name: "Background" },
+      { name: "Base Skin Color" },
+      { name: "Mouth" },
+      { name: "Nose" },
+      { name: "Eyes" },
+      { name: "Short Hair"},
+      { name: "Extra Arm" },
+      { name: "TShirts" },
+      { name: "Chakra" },
+      { name: "Accessories" },
+      ],
+      },
+      {
+      growEditionSizeTo: 570,
+      layersOrder: [
+      { name: "Background" },
+      { name: "Halo" }, 
+      { name: "Blue Base" },
+      { name: "Blue Mouth" },
+      { name: "Blue Nose" },
+      { name: "Eyes" },
+      { name: "Short Hair" },
+      { name: "Clothes" },
+      { name: "Chakra" },
+      { name: "Accessories" },
+      ],
+      },
+      {
+      growEditionSizeTo: 1000,
+      layersOrder: [
+      { name: "Background" },
+      { name: "Blue Base" },
+      { name: "Blue Mouth" },
+      { name: "Blue Nose" },
+      { name: "Eyes" },
+      { name: "Hair Color" },
+      { name: "Blue Extra Arm" },
+      { name: "TShirts" },
+      { name: "Hair Accessories" },
+      { name: "Chakra" },
+      { name: "Accessories" },
+      ],
+      },
+      {
+      growEditionSizeTo: 1400,
+      layersOrder: [
+      { name: "Background" },
+      { name: "Blue Base" },
+      { name: "Blue Mouth" },
+      { name: "Blue Nose" },
+      { name: "Eyes" },
+      { name: "Short Hair" },
+      { name: "Blue Extra Arm Holding Nothing" },
+      { name: "TShirts" },
+      { name: "Chakra" },
+      { name: "Hand Accessories" },
+      { name: "Accessories" },
+      ],
+      },
+      {
+      growEditionSizeTo: 1800,
+      layersOrder: [
+      { name: "Background" },
+      { name: "Halo" }, 
+      { name: "Blue Base" },
+      { name: "Blue Mouth" },
+      { name: "Blue Nose" },
+      { name: "Eyes" },
+      { name: "Hair Color" },
+      { name: "Blue Extra Arm Holding Nothing" },
+      { name: "Clothes" },
+      { name: "Hair Accessories" },
+      { name: "Hand Accessories" },
+      { name: "Accessories" },
+      ],
+      },
+      {
+      growEditionSizeTo: 2200,
+      layersOrder: [
+      { name: "Background" },
+      { name: "Halo" }, 
+      { name: "Green Base" },
+      { name: "Green Mouth" },
+      { name: "Green Nose" },
+      { name: "Eyes" },
+      { name: "Hair Color" },
+      { name: "Clothes" },
+      { name: "Hair Accessories" },
+      { name: "Accessories" },
+      ],
+      },
+      {
+      growEditionSizeTo: 2800,
+      layersOrder: [
+      { name: "Background" },
+      { name: "Halo" }, 
+      { name: "Green Base" },
+      { name: "Green Mouth" },
+      { name: "Green Nose" },
+      { name: "Eyes" },
+      { name: "Short Hair" },
+      { name: "Green Extra Arm" },
+      { name: "TShirts" },
+      { name: "Chakra" },
+      { name: "Accessories" },
+      ],
+      },
+      {
+      growEditionSizeTo: 3000,
+      layersOrder: [
+      { name: "Background" },
+      { name: "Green Base" },
+      { name: "Green Mouth" },
+      { name: "Green Nose" },
+      { name: "Eyes" },
+      { name: "Hair Color" },
+      { name: "Green Extra Arm Holding Nothing" },
+      { name: "TShirts" },
+      { name: "Hair Accessories" },
+      { name: "Hand Accessories" },
+      { name: "Chakra" },
+      { name: "Accessories" },
+      ],
+      },
+      {
+      growEditionSizeTo: 3400,
+      layersOrder: [
+      { name: "Background" },
+      { name: "Green Base" },
+      { name: "Green Mouth" },
+      { name: "Green Nose" },
+      { name: "Eyes" },
+      { name: "Short Hair" },
+      { name: "Green Extra Arm Holding Nothing" },
+      { name: "Clothes" },
+      { name: "Hand Accessories" },
+      { name: "Accessories" },
+      ],
+      },
+      {
+      growEditionSizeTo: 4000,
+      layersOrder: [
+      { name: "Background" },
+      { name: "Pale Base" },
+      { name: "Pale Mouth" },
+      { name: "Pale Nose" },
+      { name: "Eyes" },
+      { name: "Hair Color" },
+      { name: "Clothes" },
+      { name: "Hair Accessories" },
+      { name: "Accessories" },
+      ],
+      },
+      {
+      growEditionSizeTo: 5000,
+      layersOrder: [
+      { name: "Background" },
+      { name: "Halo" }, 
+      { name: "Pale Base" },
+      { name: "Pale Mouth" },
+      { name: "Pale Nose" },
+      { name: "Eyes" },
+      { name: "Hair Color" },
+      { name: "Pale Extra Arm" },
+      { name: "TShirts" },
+      { name: "Hair Accessories" },
+      { name: "Chakra" },
+      { name: "Accessories" },
+      ],
+      },
+      {
+      growEditionSizeTo: 5500,
+      layersOrder: [
+      { name: "Background" },
+      { name: "Pale Base" },
+      { name: "Pale Mouth" },
+      { name: "Pale Nose" },
+      { name: "Eyes" },
+      { name: "Short Hair" },
+      { name: "Pale Extra Arm Holding Nothing" },
+      { name: "TShirts" },
+      { name: "Chakra" },
+      { name: "Accessories" },
+      ],
+      },
+      {
+      growEditionSizeTo: 5800,
+      layersOrder: [
+      { name: "Background" },
+      { name: "Pale Base" },
+      { name: "Pale Mouth" },
+      { name: "Pale Nose" },
+      { name: "Eyes" },
+      { name: "Short Hair" },
+      { name: "Pale Extra Arm Holding Nothing" },
+      { name: "Clothes" },
+      { name: "Hand Accessories" },
+      { name: "Accessories" },
+      ],
+      },
+      {
+      growEditionSizeTo: 6000,
+      layersOrder: [
+      { name: "Background" },
+      { name: "Brown Base" },
+      { name: "Brown Mouth" },
+      { name: "Brown Nose" },
+      { name: "Eyes" },
+      { name: "Hair Color" },
+      { name: "Clothes", options: { blend: MODE.colorBurn, } },
+      { name: "Hair Accessories" },
+      { name: "Accessories" },
+      ],
+      },
+      {
+      growEditionSizeTo: 6300,
+      layersOrder: [
+      { name: "Background" },
+      { name: "Halo" }, 
+      { name: "Brown Base" },
+      { name: "Brown Mouth" },
+      { name: "Brown Nose" },
+      { name: "Eyes" },
+      { name: "Short Hair" },
+      { name: "Brown Extra Arm" },
+      { name: "TShirts" },
+      { name: "Chakra" },
+      { name: "Accessories" },
+      ],
+      },
+      {
+      growEditionSizeTo: 6500,
+      layersOrder: [
+      { name: "Background" },
+      { name: "Brown Base" },
+      { name: "Brown Mouth" },
+      { name: "Brown Nose" },
+      { name: "Eyes" },
+      { name: "Hair Color" },
+      { name: "Brown Extra Arm Holding Nothing", options: { blend: MODE.colorBurn, }  },
+      { name: "TShirts" },
+      { name: "Hair Accessories" },
+      { name: "Chakra" },
+      { name: "Accessories" },
+      ],
+      },
+      {
+      growEditionSizeTo: 7000,
+      layersOrder: [
+      { name: "Background" },
+      { name: "Brown Base" },
+      { name: "Brown Mouth" },
+      { name: "Brown Nose" },
+      { name: "Eyes" },
+      { name: "Short Hair", options: { blend: MODE.luminosity, } },
+      { name: "Brown Extra Arm Holding Nothing" },
+      { name: "Clothes" },
+      { name: "Hand Accessories" },
+      { name: "Accessories" },
+      ],
+      },
+      {
+      growEditionSizeTo: 7500,
+      layersOrder: [
+      { name: "Background" },
+      { name: "Base Skin Color" },
+      { name: "Mouth" },
+      { name: "Nose" },
+      { name: "Eyes" },
+      { name: "Short Hair" },
+      { name: "Clothes", options: { blend: MODE.luminosity, } },
+      { name: "Accessories" },
+      ],
+      },
+      {
+        growEditionSizeTo: 8000,
+        layersOrder: [
+      { name: "Background" },
+      { name: "Base Skin Color", options: { blend: MODE.overlay, opacity: 0.7 }},
+      { name: "Mouth", options: { blend: MODE.colorBurn, } },
+      { name: "Nose", options: { blend: MODE.softLight, } },
+      { name: "Eyes", options: { blend: MODE.luminosity, }},
+      { name: "Hair Color", opacity: 0.5 },
+      { name: "Extra Arm" },
+      { name: "TShirts", options: { blend: MODE.colorBurn, } },
+      { name: "Hair Accessories" },
+      { name: "Accessories" },
+      ],
+      },
+      {
+        growEditionSizeTo: 8200,
+        layersOrder: [
+      { name: "Background" },
+      { name: "Halo" }, 
+      { name: "Base Skin Color", options: { blend: MODE.overlay, opacity: 0.3 }},
+      { name: "Mouth", options: { blend: MODE.colorBurn, } },
+      { name: "Nose", options: { blend: MODE.softLight, } },
+      { name: "Eyes", options: { blend: MODE.luminosity, }},
+      { name: "Short Hair", options: { blend: MODE.luminosity, }},
+      { name: "Extra Arm" },
+      { name: "Clothes", options: { blend: MODE.colorBurn, } },
+      { name: "Accessories" },
+      ],
+      },
+      {
+        growEditionSizeTo: 8888,
+        layersOrder: [
+      { name: "Background" },
+      { name: "Base Skin Color", options: { blend: MODE.overlay, opacity: 0.7 }},
+      { name: "Mouth", options: { blend: MODE.colorBurn, } },
+      { name: "Nose", options: { blend: MODE.softLight, } },
+      { name: "Eyes", options: { blend: MODE.luminosity, }},
+      { name: "Hair Color", opacity: 0.5 },
+      { name: "Extra Arm" },
+      { name: "TShirts", options: { blend: MODE.colorBurn, } },
+      { name: "Hair Accessories" },
+      { name: "Chakra", options: { blend: MODE.luminosity, } },
+      { name: "Accessories" },
+      ],
+      },                           
 ];
-
 const shuffleLayerConfigurations = true;
-
 const debugLogs = false;
-
 const format = {
-  width: 512,
-  height: 512,
+  width: 1080,
+  height: 1080,
   smoothing: false,
 };
 
 const extraMetadata = {
-  external_url: "https://codecats.xyz", // Replace with your website or remove this line if you do not have one.
+  creator: "GabeYoga",
+  artist: 'Gabriel Azoulay & Kenjie Cuarto', 
+  external_url: 'https://www.NFTYoginis.com',
 };
 
 // NFTPort Info
 
 // ** REQUIRED **
 const AUTH = process.env.NFTPORT_API_KEY; // Set this in the .env file to prevent exposing your API key when pushing to Github
-const LIMIT = 2; // Your API key rate limit
+const LIMIT = 10; // Your API key rate limit
 const CHAIN = 'rinkeby'; // only rinkeby, polygon, or ethereum
 
 // REQUIRED CONTRACT DETAILS THAT CANNOT BE UPDATED LATER!
-const CONTRACT_NAME = 'CRYPTOPUNKS';
-const CONTRACT_SYMBOL = 'CP';
+const CONTRACT_NAME = 'NFTYoginis Goddess Collection';
+const CONTRACT_SYMBOL = 'GDS';
 const METADATA_UPDATABLE = true; // set to false if you don't want to allow metadata updates after minting
-const OWNER_ADDRESS = 'YOUR WALLET ADDRESS HERE';
-const TREASURY_ADDRESS = 'YOUR WALLET ADDRESS HERE';
-const MAX_SUPPLY = 5000; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
-const MINT_PRICE = 0.01; // Minting price per NFT. Rinkeby = ETH, Ethereum = ETH, Polygon = MATIC. CANNOT BE UPDATED!
+const OWNER_ADDRESS = '0xa23612723c5cbc52729d95F409B2E2bbd33f6859';
+const TREASURY_ADDRESS = '0xa23612723c5cbc52729d95F409B2E2bbd33f6859';
+const MAX_SUPPLY = 8888; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
+const MINT_PRICE = 0.001; // Minting price per NFT. Rinkeby = ETH, Ethereum = ETH, Polygon = MATIC. CANNOT BE UPDATED!
 const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PUBLIC_MINT_START_DATE = "2022-03-20T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
+const PUBLIC_MINT_START_DATE = "2022-10-31T16:20:08+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2022-08-02T13:30:08+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
-const ROYALTY_ADDRESS = "0xd8B808A887326F45B2D0cd999709Aa6264CeF919"; // Address that will receive the royalty
+const ROYALTY_ADDRESS = "0xa23612723c5cbc52729d95F409B2E2bbd33f6859"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
-const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
+const PRESALE_WHITELISTED_ADDRESSES = ["0xa23612723c5cbc52729d95F409B2E2bbd33f6859", "0xfe7c160823Ab5EFCE0b1e8366d1D2226E5067C80","0x0Cb8C6652Ce440FD89d427B7F7Cf64942Cb02B21"]; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
 let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
@@ -74,8 +401,8 @@ let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually inclu
 // Generic Metadata is optional if you want to reveal your NFTs
 const GENERIC = true; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
 const GENERIC_TITLE = CONTRACT_NAME; // Replace with what you want the generic titles to say if you want it to be different from the contract name.
-const GENERIC_DESCRIPTION = "REPLACE THIS"; // Replace with what you want the generic descriptions to say.
-const GENERIC_IMAGE = "https://ipfs.io/ipfs/QmUf9tDbkqnfHkQaMdFWSGAeXwVXWA61pFED7ypx4hcsfh"; // Replace with your generic image that will display for all NFTs pre-reveal.
+const GENERIC_DESCRIPTION = "Which Goddess will you get?"; // Replace with what you want the generic descriptions to say.
+const GENERIC_IMAGE = "https://ipfs.io/ipfs/bafkreifzbr2i2qyslniwz7nboyhrv7ik66la44fapj7lpr7nbydju4tc2u"; // Replace with your generic image that will display for all NFTs pre-reveal.
 
 // Automatically set contract address if deployed using the deployContract.js script
 try {
@@ -98,7 +425,7 @@ const solanaMetadata = {
   creators: [
     {
       address: "7fXNuer5sbZtaTEPhtJ5g5gNtuyRoKkvxdjEjEnPN4mC",
-      share: 100,
+      share: 1,
     },
   ],
 };
@@ -135,23 +462,21 @@ const background = {
 };
 
 const rarityDelimiter = "#";
-
-const uniqueDnaTorrance = 10000;
-
+const uniqueDnaTorrance = 8888;
 const preview = {
-  thumbPerRow: 5,
+  thumbPerRow: 10,
   thumbWidth: 50,
   imageRatio: format.height / format.width,
-  imageName: "preview.png",
+  imageName: "NFTYoginis Goddess Collection.png",
 };
 
 const preview_gif = {
-  numberOfImages: 5,
+  numberOfImages: 20,
   order: "ASC", // ASC, DESC, MIXED
   repeat: 0,
   quality: 100,
   delay: 500,
-  imageName: "preview.gif",
+  imageName: "NFTYoginis Goddess Collection.gif",
 };
 
 module.exports = {
--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,22 +1,22 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; // Replace with your own contract address
+const contractAddress = "0x07e558819a1BBaa4496e6a3ECc06b4786d6303EB"; // Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby, polygon, or ethereum
 
-const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
-const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
+const welcome_h1 = "Welcome to the Yoginis Goddess empower the Metaverse Project!!";
+const welcome_h2 = "Please connect to MetaMask to Get Started";
+const welcome_p = 'When you mint an NFTYoginis Goddess you spread yoga within the Web 3 Universe. Each Goddess grants access to member only benefits, with lifetime access to events in the Metaverse and the Real World.';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
 const h2_presale_coming_soon = "Pre-Sale Minting Countdown";
 const h2_presale_mint = "Public Minting Countdown";
-const p_presale_coming_soon = "We are working hard to launch the NFT Collection. Stay tuned for updates!";
+const p_presale_coming_soon = "The Goddess light is spreading. Stay tuned for updates!";
 const p_presale_mint_not_whitelisted = "You are not whitelisted for the pre-sale.. 😢";
 const p_presale_mint_whitelisted = "You're on the whitelist for the pre-sale! 🎉";
 const p_presale_mint_already_minted = "You've already claimed your whitelist mint. Thank you! 🎉";
 const p_public_mint = "No whitelist needed. Public minting is now open! 🎉";
 const button_presale_coming_soon = "Get on the Whitelist";
-const button_presale_mint_whitelisted = "Mint Your Special NFT";
+const button_presale_mint_whitelisted = "Mint Your Unique Goddess NFT";
 const button_presale_mint_not_whitelisted = "Get on the Whitelist";
-const button_presale_already_minted = "Join The Community";
+const button_presale_already_minted = "Join The NFTYoginis Community";
 const button_public_mint = "Mint Your NFT";
 const mint_failed = "Minting failed. 😢 Please try again.";
