ef8c229aa9a979be7b79b5daf074a5a63175ea98
NFT Yoginis <gabeyoga@gmail.com>
2022-07-08 23:15:28 +0700

    text

--- backend/src/config.js
+++ backend/src/config.js
@@ -388,7 +388,7 @@ const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a singl
 const PUBLIC_MINT_START_DATE = "2022-10-31T16:20:08+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = "2022-08-02T13:30:08+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2022-07-07T13:30:08+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0xa23612723c5cbc52729d95F409B2E2bbd33f6859"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
